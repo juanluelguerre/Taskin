03eeec179dbd9e5fb07490154914cef6d7ed94e6
Juan Luis Guerrero <jlguerrero@gmail.com>
2018-06-17 22:51:48 +0200

    TaskDetail page added

---
+++ src/ElGuerre.Taskin.Models/Priority.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------------------------
+// <copyright file="Priority.cs" Author="Juan Luis Guerrero Minero" www="elGuerre.com">
+//     Copyright (c) elGuerre.com. All rights reserved.
+// </copyright>
+// ---------------------------------------------------------------------------------
+namespace ElGuerre.Taskin.Models
+{
+    public enum Priority
+    {
+        Low = 1,
+        Normal = 2,
+        High = 3
+    }
+}
---
+++ src/ElGuerre.Taskin.Models/TaskType.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------------------------------------
+// <copyright file="TaskType.cs" Author="Juan Luis Guerrero Minero" www="elGuerre.com">
+//     Copyright (c) elGuerre.com. All rights reserved.
+// </copyright>
+// ---------------------------------------------------------------------------------
+namespace ElGuerre.Taskin.Models
+{
+    public enum TaskType
+    {
+        ToDo = 0,
+        CallTo = 1,
+        EmailTo = 2,
+        TalkTo = 3,
+        LunchWith = 4,
+        Holidays = 5,
+        Travel = 6,
+        Read = 7,
+        Sport = 8,
+        LinkTo = 9,
+
+        // Add more if need it
+
+        Others = 20
+    }
+}
