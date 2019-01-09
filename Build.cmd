e41cc16c0af6283cc60e6cae9e6ae5dfc13e1151
Juan Luis Guerrero Minero <jlguerrero@gmail.com>
2019-01-09 23:21:21 +0100

    + buid.sh

--- Build.cmd
+++ Build.cmd
@@ -1,17 +1,19 @@
 @echo off
 
-CLS
+REM dotnet build-server shutdown
+REM del .\.sonarqube /S /Q
+
 setlocal
 
-set /p token=<sonar.txt
-IF [%1]==[] SET SLN="./ElGuerre.Taskin.Full.sln"
+set /p TOKEN=<sonar.token
+IF [%1]==[] SET SLN="./ElGuerre.Taskin.Api.sln"
 IF NOT [%1]==[] SET SLN=%1
 
-:: dotnet sonarscanner begin /k:"company:project" /n:"Project" /v:"#.#.#" /o:"companyname" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="%token%" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*" /d:sonar.coverage.exclusions="Project.Tests/**,**/*Tests.cs" /d:sonar.cs.opencover.reportsPaths="%cd%\lcov.opencover.xml"
-::dotnet SonarScanner.MSBuild.exe begin /k:"Taskin" /d:sonar.organization="juanluelguerre-github" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="%token%"
-dotnet sonarScanner begin /k:"Taskin" /d:sonar.organization="juanluelguerre-github" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="%token%" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*" /d:sonar.coverage.exclusions="test/**" /d:sonar.cs.opencover.reportsPaths="%cd%\lcov.opencover.xml"
-dotnet restore %sln%
-dotnet build %sln%
-dotnet test test/UnitTets/ElGuerre.Taskin.Api.Services.Tests/ElGuerre.Taskin.Api.Services.Tests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=\"opencover,lcov\" /p:CoverletOutput=../../lcov
-dotnet sonarscanner end /d:sonar.login="%token%"
-::dotnet SonarScanner.MSBuild.exe end /d:sonar.login="%token%"
+REM dotnet tool install --global dotnet-sonarscanner
+REM dotnet tool install --global dotnet-reportgenerator-globaltool
+
+dotnet sonarScanner begin /k:"Taskin" /d:sonar.organization="juanluelguerre-github" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="%TOKEN%" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*" /d:sonar.coverage.exclusions="test/**" /d:sonar.cs.opencover.reportsPaths="%cd%\lcov.opencover.xml"
+dotnet restore %SLN%
+dotnet build %SLN%
+dotnet test test/ElGuerre.Taskin.Api.Tests/ElGuerre.Taskin.Api.Tests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=\"opencover,lcov\" /p:CoverletOutput=../lcov
+dotnet sonarscanner end /d:sonar.login="%TOKEN%"
