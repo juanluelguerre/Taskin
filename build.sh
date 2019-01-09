9c98043dd63fdb50964886fbed6aeaf33799d828
Juan Luis Guerrero Minero <jlguerrero@gmail.com>
2019-01-09 23:26:07 +0100

    *buid.sh

--- build.sh
+++ build.sh
@@ -11,15 +11,11 @@ fi
 # dotnet tool install --global dotnet-sonarscanner
 # dotnet tool install --global dotnet-reportgenerator-globaltool
 
-#dotnet sonarScanner begin /k:'Taskin' /d:sonar.organization='juanluelguerre-github' /d:sonar.host.url='https://sonarcloud.io' /d:sonar.login="$TOKEN" /d:sonar.language='cs' /d:sonar.exclusions='**/bin/**/*,**/obj/**/*' /d:sonar.coverage.exclusions='test/**' /d:sonar.cs.opencover.reportsPaths="$(pwd)\lcov.opencover.xml"
-#dotnet restore $SLN
+dotnet sonarScanner begin /k:'Taskin' /d:sonar.organization='juanluelguerre-github' /d:sonar.host.url='https://sonarcloud.io' /d:sonar.login="$TOKEN" /d:sonar.language='cs' /d:sonar.exclusions='**/bin/**/*,**/obj/**/*' /d:sonar.coverage.exclusions='test/**' /d:sonar.cs.opencover.reportsPaths="$(pwd)\lcov.opencover.xml"
+# dotnet restore $SLN
 # dotnet build $SLN # It doesn't woks on MAC for msbuild 16. Issue: https://github.com/SonarSource/sonar-scanner-msbuild/issues/649
 # Neither force for: dotnet msbuild /toolsversion:15 ElGuerre.Taskin.Api.sln
-# msbuild $SLN # It works 
-
-# dotnet test './test/ElGuerre.Taskin.Api.Tests/ElGuerre.Taskin.Api.Tests.csproj' /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutputFormat=lcov /p:CoverletOutput='../lcov'
+msbuild $SLN # It works 
 dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=OpenCover /p:CoverletOutput=./TestResults/ ./test/ElGuerre.Taskin.Api.Tests/ElGuerre.Taskin.Api.Tests.csproj
 reportgenerator -reports:$(pwd)/TestResults/coverage.opencover.xml -targetdir ./TestResults/Reports/ -reportTypes:"HTMLInline;Cobertura"
-
-
-# dotnet sonarScanner end /d:sonar.login="$TOKEN"
+dotnet sonarScanner end /d:sonar.login="$TOKEN"
